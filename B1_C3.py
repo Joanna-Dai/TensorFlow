7f543d97a4c14e9cd410fbccdbdb3c56974b30a9
Joanna Dai <joan.di1223@gmail.com>
2021-08-29 10:08:05 +0800

    20210829

--- B1_C3.py
+++ B1_C3.py
@@ -41,12 +41,17 @@ model.compile(optimizer='adam',
 #1st conv layer: 28x28 --conv2D (3x3 filter, left/right/top/bottom pixel has no full neighours to conv)--> (28-2)x(28-2)
 #2nd pooling layer: 26x26 --pooling (2x2 max) --> 26/2 x 26/2
 #3rd conv layer: 13x13--conv23 (3x3 filter) --> (13-2) x (13-2)
-#4th polling layer: 11x11--pooling (2x2 max)--> rounddown(11/2) x roundown (11/2)
+#4th pooling layer: 11x11--pooling (2x2 max)--> rounddown(11/2) x roundown (11/2)
 
-#parameters:
-
-
-
+# parameters:
+#1st conv layer: # para = 64 convolutions x (3x3 weights for each conv to learn+1 bias)=64x10=640
+#2nd pooling layer: 13x13, 64 of them; # para = 0 (nothing to learn but just to reduce the image)
+#3rd conv layer: # para = 64 convolutions x previous 64 conv x 9 para + 64 convolutions x 1 bias = 36928
+#4th pooling layer: 5x5, 64 of them; # para=0 (nothing to learn but just to reduce the image)
+#5th flatten: take images to numeric value
+#6th dense layer: 128 neurons; # para = 128 x (5x5x64) +128 x 1 bias =204928
+#7th dense layer: 10 neuron; # para  = 10 x 128 previous output + 10 x 1bias= 1290
+#total # learned para = 640 + 0 + 36928 + 0 + 204928 + 1290 = 243786 --> the network requires us to learn the best set of 243786 parameters to match the input images to their labels
 
 model.summary()
 
