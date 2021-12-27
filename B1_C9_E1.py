1ea999aa8f2265cc23e846457aff2789d17db3d3
Joanna Dai <joan.di1223@gmail.com>
2021-12-27 15:30:43 +0800

    20211227

--- B1_C9_E1.py
+++ B1_C9_E1.py
@@ -4,6 +4,8 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
+# define all functions
+
 # attributes of TS
 # trend: move in a specific direction
 def trend(time, slope=0):
@@ -27,6 +29,16 @@ def noise(time, noise_level=1, seed=None):
                                        # seed used to initialize the pseudo-random number generator. Can be any integer between 0 and 2**32 - 1 inclusive
     return rnd.randn(len(time)) * noise_level
 
+
+# graph: plot the time series
+def plot_series(time, series, format="-", start=0, end=None):
+    plt.plot(time[start:end], series[start:end], format)
+    plt.xlabel("Time")
+    plt.ylabel("Value")
+    plt.grid(True)
+    plt.show()
+
+
 # set parameters for trend, seasonality and noise
 time = np.arange(4*365+1, dtype="float32") # 4-year time points starting from 0, numpy.arange(start=0, stop, step=1, dtype = None)
 baseline = 10 # intercept
@@ -37,13 +49,16 @@ noise_level = 6 # amplify noise
 series = baseline + trend(time, slope) + seasonality(time, period=365, amplitude=amplitude)
 # update series with noise
 series += noise(time, noise_level, seed=42)
-
 # plot the time series
-def plot_series(time, series, format="-", start=0, end=None):
-    plt.plot(time[start:end], series[start:end], format)
-    plt.xlabel("Time")
-    plt.ylabel("Value")
-    plt.grid(True)
-    plt.show()
-
 plot_series(time, series)
+
+
+# split the data into training and validation sets
+split_time = 1000
+time_train = time[:split_time] # the first 1000 time points [0,1000)
+x_train = series[:split_time] # returned series for training points
+time_valid = time[split_time:] # the rest 461 time points starting from 1000, which is [1000, 1460] in this case
+x_valid = series[split_time:] # returned series for validation points
+
+# naive forecast: predict series from a split time period onwards
+naive_forecast = series[split_time-1: -1]
