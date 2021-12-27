513adf5a2653c6f4e7473cd6eab6baf835699ef6
Joanna Dai <joan.di1223@gmail.com>
2021-12-27 11:56:46 +0800

    20211227

--- B1_C8_E2.py
+++ B1_C8_E2.py
@@ -26,21 +26,23 @@ data = open(os.path.join(folder_dir, file_name)).read()
 
 # windowing the data: to extend the size of dataset without adding new songs: input_sequences # = (number_of_words - window_size) x window_size
 #   instead of line by line, we create a "window" by treating all lines as one long, continuous text and move the window forward one word to get the next input sequences
-window_size = 10
 sentences = []
 alltext = []
+# tune-able hyperparameter: smaller window size --> more data, but there will be fewer words (sequence becomes smaller) to give a label and may end up with nonsensical poetry
 max_sequence_len = 6
 # converted to lower case
 corpus = data.lower()
+alltext.append(corpus)
 # split into an array of words
 words = corpus.split(" ")
+print(len(words))
 
 # i is the index of the first word of the smaller sequence
 #   the largest possible value for i = range_size (note max_sequence_len = size of the moving window/smaller sequence)
 range_size = len(words)-max_sequence_len
 for i in range(0, range_size):
 	thissentence=""
-    # loop through each word and make sentence of each word from the current index up to the current index plus max_sequence_len (window size)
+    # loop through each word and make sentence of each word from the current index up to the current index plus max_sequence_len (window size) --> more data & slower speed
 	for word in range(0, max_sequence_len-1):
 		word = words[i+word]
 		thissentence = thissentence + word
@@ -48,11 +50,14 @@ for i in range(0, range_size):
     # add each newly constructed sentences into sentences array
 	sentences.append(thissentence)
 # on above way, we have number of smaller sequences (for training): range_size x max_sequence_len = (num_words - window_size) x window_size
+print(sentences)
 
 oov_tok = "<OOV>"
-vocab_size=2700
+# tune-able hyperparameter: vocab_size for training (hereby the next word will be across 2700 words)
+vocab_size = 2700
 tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok, split=" ", char_level=False)
 tokenizer.fit_on_texts(alltext)
+# 2691 unique words
 total_words = len(tokenizer.word_index) + 1
 
 print(tokenizer.word_index)
@@ -61,8 +66,8 @@ print(total_words)
 
 # split the sentence/line into multiple smaller sequences
 input_sequences = []
-# loop each line of the songs
-for line in corpus:
+# loop each line in the sentences group we created
+for line in sentences:
     # tokenize the line and change list "[[ ]]" to "[ ]" by putting [0]
 	token_list = tokenizer.texts_to_sequences([line])[0]
     # loop smaller sequences of the line and start from (word 0 + word 1)
@@ -74,9 +79,7 @@ for line in corpus:
 
 
 # pad all input_sequences into a regular shape
-max_sequence_len = max(len(x) for x in input_sequences) # find the longest sentence in the input sequences first
 input_sequences = np.array(pad_sequences(input_sequences, maxlen=max_sequence_len, padding='pre'))
-
 
 # split smaller sequences into features (the sequence without last word) and label (the last word of the sequence)
 xs, labels = input_sequences[:, :-1], input_sequences[:, -1]
@@ -86,17 +89,24 @@ ys = tf.keras.utils.to_categorical(labels, num_classes=total_words)
 
 # model hierarchy
 model = Sequential()
-model.add(Embedding(total_words, 8))
+# tune-able hyperparameter: change dimension from 8 to 16
+model.add(Embedding(total_words, 16))
 # improve model by using multiple stacked LSTMs
-model.add(Bidirectional(LSTM(max_sequence_len-1, return_sequences='True')))
-model.add(Bidirectional(LSTM(max_sequence_len-1)))
+# tune-able hyperparameter: the number of LSTMs --> change from window size (max_sequence_len=6) to 32
+model.add(Bidirectional(LSTM(32, return_sequences='True')))
+model.add(Bidirectional(LSTM(32)))
 model.add(Dense(total_words, activation='softmax'))
 # model guess approach
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-# 1000-epoch result: model accuracy=64%, loss=1.51
+# tune-able hyperparameter: upper the learning rate on the adam optimizer
+# note: if encounter direct copy from the sentences in corpus --> best to reduce learning rate and decrease the number of LTSMs
+adam = Adam(lr=0.01)
+# model accuracy is not the best measurement for poetry: subjective examination is required --> no hard-and-fast rule to follow to determine whether the model is good or bad
+model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
+# 1000-epoch result (pre-tune): model accuracy=64%, loss=1.51
+# 100-epoch result (post-tune):
 # note: result of different users can be different due to the random initialization of the neurons will impact the final scores
-history = model.fit(xs, ys, epochs=1000, verbose=1)
-
+history = model.fit(xs, ys, epochs=100, verbose=1)
+model.save("bidiirish2.h5")
 
 # graph epoch against model accuracy
 import matplotlib.pyplot as plt
@@ -107,6 +117,8 @@ def plot_graphs(history, string):
   plt.ylabel(string)
   plt.show()
 
+plot_graphs(history, 'accuracy')
+plot_graphs(history, 'loss')
 
 # create next words for seed text
 seed_text = "sweet jeremy saw dublin"
