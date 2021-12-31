1ee2b2e3e4021126db5c9079eadeb77eee716625
Joanna Dai <joan.di1223@gmail.com>
2021-12-31 15:48:27 +0800

    20211231

---
+++ B1_C11_E1.py
@@ -0,0 +1,98 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_series(time, series, format="-", start=0, end=None):
+    plt.plot(time[start:end], series[start:end], format)
+    plt.xlabel("Time")
+    plt.ylabel("Value")
+    plt.grid(True)
+
+def trend(time, slope=0):
+    return slope * time
+
+def seasonal_pattern(season_time):
+    """Just an arbitrary pattern, you can change it if you wish"""
+    return np.where(season_time < 0.4,
+                    np.cos(season_time * 2 * np.pi),
+                    1 / np.exp(3 * season_time))
+
+def seasonality(time, period, amplitude=1, phase=0):
+    """Repeats the same pattern at each period"""
+    season_time = ((time + phase) % period) / period
+    return amplitude * seasonal_pattern(season_time)
+
+def noise(time, noise_level=1, seed=None):
+    rnd = np.random.RandomState(seed)
+    return rnd.randn(len(time)) * noise_level
+
+# set parameters for time series
+time = np.arange(4 * 365 + 1, dtype="float32")
+baseline = 10
+series = trend(time, 0.1)
+baseline = 10
+amplitude = 20
+slope = 0.09
+noise_level = 5
+
+# Create the series
+series = baseline + trend(time, slope) + seasonality(time, period=365, amplitude=amplitude)
+# Update with noise
+series += noise(time, noise_level, seed=42)
+
+# split time series to train and vaidation sets
+split_time = 1000
+time_train = time[:split_time]
+x_train = series[:split_time]
+time_valid = time[split_time:]
+x_valid = series[split_time:]
+
+# set parameters for windowed dataset
+window_size = 20
+batch_size = 32
+shuffle_buffer_size = 1000
+
+def windowed_dataset(series, window_size, batch_size, shuffle_buffer):
+  # add tf.expand_dims to define single-dimension windowed dataset as a 1D tensor for CNN usage
+  series = tf.expand_dims(series, axis=-1)
+  dataset = tf.data.Dataset.from_tensor_slices(series)
+  dataset = dataset.window(window_size + 1, shift=1, drop_remainder=True)
+  dataset = dataset.flat_map(lambda window: window.batch(window_size + 1))
+  dataset = dataset.shuffle(shuffle_buffer).map(lambda window: (window[:-1], window[-1]))
+  dataset = dataset.batch(batch_size).prefetch(1)
+  return dataset
+
+# 1D tensor windowed dataset
+dataset = windowed_dataset(x_train, window_size, batch_size, shuffle_buffer_size)
+
+model = tf.keras.models.Sequential([
+    # add a convolutional layer before the dense layer
+    #   filters: # of filters the layer to learn
+    #   kernal_size: the size of the filter
+    #   strides: step size filter takes to scan across the list (typically 1)
+    #   padding="casual": take data from the current and he previous (not future) time steps to pad the lost values
+    #   activation = "relu": effectively reject negative values coming out of the layers
+    #   input_shape: input data passed to the network, must-to-specify for the first layer
+    tf.keras.layers.Conv1D(filters=128, kernel_size=3,
+                      strides=1, padding="causal",
+                      activation="relu",
+                      input_shape=[None, 1]),
+    tf.keras.layers.Dense(28, activation="relu"),
+    tf.keras.layers.Dense(10, activation="relu"),
+    tf.keras.layers.Dense(1),
+])
+
+optimizer = tf.keras.optimizers.SGD(lr=1e-5, momentum=0.5)
+model.compile(loss="mse", optimizer=optimizer)
+history = model.fit(dataset, epochs=100,  verbose=1)
+
+
+# predict an entire series based on the model, with the specified window size
+def model_forecast(model, series, window_size):
+    ds = tf.data.Dataset.from_tensor_slices(series)
+    ds = ds.window(window_size, shift=1, drop_remainder=True)
+    ds = ds.flat_map(lambda w: w.batch(window_size))
+    ds = ds.batch(32).prefetch(1)
+    forecast = model.predict(ds)
+    return forecast
+
