8a8a695c273502c7ee809cb4ee0ee3faf0a9af83
Joanna Dai <joan.di1223@gmail.com>
2022-01-11 09:05:02 +0800

    20220111

---
+++ B1_C12_E2.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+import tensorflow as tf
+import tensorflow_hub as hub
+import tensorflow_datasets as tfds
+
+
+def format_image(image, label):
+    image = tf.image.resize(image, (224, 224)) / 255.0
+    return  image, label
+
+
+(raw_train, raw_validation, raw_test), metadata = tfds.load(
+    'cats_vs_dogs',
+    split=['train[:80%]', 'train[80%:90%]', 'train[90%:]'],
+    with_info=True,
+    as_supervised=True,
+)
+
+num_examples = metadata.splits['train'].num_examples
+num_classes = metadata.features['label'].num_classes
+print(num_examples)
+print(num_classes)
+
+BATCH_SIZE = 32
+train_batches = raw_train.shuffle(num_examples // 4).map(format_image).batch(BATCH_SIZE).prefetch(1)
+validation_batches = raw_validation.map(format_image).batch(BATCH_SIZE).prefetch(1)
+test_batches = raw_test.map(format_image).batch(1)
+
+for image_batch, label_batch in train_batches.take(1):
+    pass
+
+image_batch.shape
