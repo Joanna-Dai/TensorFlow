947314d660854834ad4ee08497656bb9f00852d1
Joanna Dai <joan.di1223@gmail.com>
2022-01-15 20:09:27 +0800

    20220115

--- B1_C12_E2.py
+++ B1_C12_E2.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pylab as plt
 
-# step 1: build and save the model
+# Step 1: build and save the model
 
 import tensorflow as tf
 import tensorflow_hub as hub
@@ -33,7 +33,59 @@ train_batches = raw_train.shuffle(num_examples // 4).map(format_image).batch(BAT
 validation_batches = raw_validation.map(format_image).batch(BATCH_SIZE).prefetch(1)
 test_batches = raw_test.map(format_image).batch(1)
 
-for image_batch, label_batch in train_batches.take(1):
-    pass
+# create feature_extractor layer, which would be the first layer of neural network
+module_selection = ("mobilenet_v2", 224, 1280)
+handle_base, pixels, FV_SIZE=module_selection
+MODULE_HANDLE ="https://tfhub.dev/google/tf2-preview/{}/feature_vector/4".format(handle_base)
+IMAGE_SIZE = (pixels, pixels)
+print("Using {} with input size {} and output dimension {}".format(MODULE_HANDLE, IMAGE_SIZE, FV_SIZE))
+feature_extractor = hub.KerasLayer(MODULE_HANDLE,
+                                   input_shape=IMAGE_SIZE + (3,),
+                                   output_shape=[FV_SIZE],
+                                   trainable=False)
 
-#image_batch.shape
+print("Building model with", MODULE_HANDLE)
+
+# model architecture
+model = tf.keras.Sequential([
+        feature_extractor,
+        tf.keras.layers.Dense(num_classes, activation='softmax')
+])
+
+model.summary()
+
+model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+
+EPOCHS = 5
+
+# train_acc=0.994, val_acc=0.988
+hist = model.fit(train_batches,
+                 epochs=EPOCHS,
+                 validation_data=validation_batches)
+
+# save the model out
+CATS_VS_DOGS_SAVED_MODEL = "exp_saved_model"
+tf.saved_model.save(model, CATS_VS_DOGS_SAVED_MODEL)
+
+
+# Step 2: covert the model to TensorFlow lite
+converter = tf.lite.TFLiteConverter.from_saved_model(CATS_VS_DOGS_SAVED_MODEL)
+
+converter.optimizations = [tf.lite.Optimize.DEFAULT]
+
+def representative_data_gen():
+    for input_value, _ in test_batches.take(100):
+        yield [input_value]
+
+converter.representative_dataset = representative_data_gen
+converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+
+tflite_model = converter.convert()
+tflite_model_file = 'converted_model.tflite'
+
+with open(tflite_model_file, "wb") as f:
+    f.write(tflite_model)
+
+tflite_model_file = 'converted_model_withoptimizationsandquant.tflite'
